089975af019ec6aaa9e4f26968ac05dc36d83c85
Mohamed Abdul <mohamedyousufmo@gmail.com>
2023-02-23 12:50:53 -0800

    added test cases for cosine

--- BERT-Project/main.py
+++ BERT-Project/main.py
@@ -1,11 +1,19 @@
 from utills.SentenceUtill import SentenceUtill
+from utills.VectorUtill import VectorUtill
 
 sentenceUtill = SentenceUtill()
+vectorUtill = VectorUtill()
 
+## test sentence breaker
 text = 'So yesterday I was there. But i do no know what happend, so i left. on Monday I was like What! Do you know what? so tuesday I was like whatever.'
+expectedList = ['So yesterday I was there.', ' But i do no know what happend, so i left.', ' on Monday I was like What!', ' Do you know what?', ' so tuesday I was like whatever.']
 
-'''
-  Expected 
-  ['So yesterday I was there.', ' But i do no know what happend, so i left.', ' on Monday I was like What!', ' Do you know what?', ' so tuesday I was like whatever.']
-'''
-print(sentenceUtill.extractSentencesFromParagraph(text))
+assert expectedList == sentenceUtill.extractSentencesFromParagraph(text)
+
+
+## test cosine similarity method
+docVectorOne = [1,1,1,1,1,0,0]
+docVectorTwo = [0,0,1,1,0,1,1]
+
+
+assert 0.44721 == round(vectorUtill.computeCosineSimilarity(docVectorOne,docVectorTwo), 5)
