44929fbf72e10f974f7b52adeccc12feb6801ca0
Mohamed Abdul <mohamedyousufmo@gmail.com>
2023-02-23 11:06:43 -0800

    added sentence extractor

--- BERT-Project/utills/SentenceUtill.py
+++ BERT-Project/utills/SentenceUtill.py
@@ -5,5 +5,27 @@ class SentenceUtil:
     def __int__(self):
         pass
 
-    def extractSentencesFromParagraph(self, paragraph: str):
-        pass
+    def extractSentencesFromParagraph(self, paragraph: str) -> list:
+        sentenceList = []
+        charIndex = 0
+        currentSentence = ""
+
+        while charIndex < len(paragraph):
+            currentChar = paragraph[charIndex]
+            ### append the charc
+            currentSentence = currentSentence + currentChar
+            if self._isSentenceEnding(currentChar):
+                ## push the sentence to the list
+                sentenceList.append(currentSentence)
+                ## reset for new sentence to the
+                currentSentence = ""
+        ## append the last sentence
+        if len(currentSentence) > 0:
+            sentenceList.append(currentSentence)
+        return sentenceList
+
+    def _isSentenceEnding(self, char:str):
+        endingPunctuations = [".", ",", "!"]
+        if char in endingPunctuations:
+            return True
+        return False
