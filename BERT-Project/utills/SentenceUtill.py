09c0684436d9f199329d6095778875e7bbfa3a70
Mohamed Abdul <mohamedyousufmo@gmail.com>
2023-02-22 22:59:35 -0800

    intial draft

---
+++ BERT-Project/utills/SentenceUtill.py
@@ -0,0 +1,9 @@
+
+'''This class will parse paragraph into sentences'''
+class SentenceUtil:
+
+    def __int__(self):
+        pass
+
+    def extractSentencesFromParagraph(self, pragraph):
+        pass
