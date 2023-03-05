369eacb1e0d882ea214f692fd39e799c021645d3
Mohamed Abdul <mohamedyousufmo@gmail.com>
2023-03-04 16:15:46 -0800

    added evaluator

---
+++ BERT-Project/evaluator/StoryQualityEvaluator.py
@@ -0,0 +1,10 @@
+
+'''
+    This class will process the evaluation of the story based on the method
+    proposed in the paper. Here where we will utilize all neccessary tools such
+    as BertProcessor and other utills
+'''
+class StoryQualityEvaluator:
+
+    def __init__(self):
+        pass
